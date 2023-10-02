ce389bcacfd6d87fe55278929dbb64c53ddacc98
Phil Tegg <phil.tegg@gmail.com>
2023-10-02 09:32:18 +0100

    Test.

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -2,34 +2,9 @@
 
 import pytest
 import logging
-import logging.config
 from src.logger import instantiate_logger
 
 
-@pytest.fixture
-def logger():
-    return instantiate_logger()
-
-
-def test_instantiate_logger(logger):
-    # Check if the logger is correctly configured
+def test_instantiate_logger():
+    logger = instantiate_logger()
     assert isinstance(logger, logging.Logger)
-
-    # Check if the logger has a Console Handler and File Handler
-    console_handler_exists = False
-    file_handler_exists = False
-
-    logger.debug(logger.handlers)
-    for handler in logger.handlers:
-        # Check console handler
-        if isinstance(handler, logging.StreamHandler):
-            console_handler_exists = True
-            assert handler.level == logging.DEBUG
-        # Check file handler
-        if isinstance(handler, logging.FileHandler):
-            file_handler_exists = True
-            assert handler.level == logging.DEBUG
-
-    # Check handlers exist
-    assert console_handler_exists
-    assert file_handler_exists
